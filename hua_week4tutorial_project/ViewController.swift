ff7d2b517fc3aeb1debf71b852d881c28ab2481e
Hua Cai <hcai1@imach-57.cis.utas.edu.au>
2018-05-08 17:36:11 +1000

    Initial Commit

---
+++ hua_week4tutorial_project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hua_week4tutorial_project
+//
+//  Created by Hua Cai on 8/5/18.
+//  Copyright © 2018 Hua Cai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
